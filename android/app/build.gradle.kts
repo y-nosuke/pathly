4b2221619c92e847dd2874de95b1cbd91e3748e7
nakao yoichi <yoichi@ad.physicist00.jp>
2025-09-09 08:28:34 +0900

    jvmTargetの警告を修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,11 +47,13 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
-  kotlinOptions {
-    jvmTarget = "11"
+  kotlin {
+    compilerOptions {
+      jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
   }
   buildFeatures {
     compose = true
